9e71bf498d0c41d149b01e4d2f29ce6c4613da9f
puran <mylarmpuran@gmail.com>
2024-04-13 15:37:31 +0530

    foodservice

---
+++ src/controllers/foodController.js
@@ -0,0 +1,92 @@
+const foodService = require('../services/foodService.js');
+const restaurantService = require('../services/RestaurantService.js');
+const userService = require('../services/user.service.js');
+
+const searchFood = async(req,res)=> {
+    try {
+        const {name} = req.query;
+        const menuItem = await foodService.searchFood(name);
+        res.status(200).json(menuItem);
+    } catch (error) {
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+
+const getMenuItemByRestaurantId = async(req, res) => {
+    try{
+        const { restaurantId } = req.params;
+        const { vegetarian , seasonal, nonveg, food_category } = req.query;
+        const menuItems = await foodService.getRestaurantsFood(
+            restaurantId,
+            vegetarian,
+            nonveg,
+            seasonal,
+            food_category
+        );
+        res.status(200).json(menuItems);
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(400).json({error: error.message});
+            
+        } else {
+            res.status(500).json({error: "Internal server error"});
+        }
+    }
+}
+
+const createItem = async(req,res) => {
+    try {
+        const item = req.body
+        const user = req.user;
+        const restaurant = await restaurantService.findRestaurantById(
+            item.restaurantId
+        );
+        const menuItem = await foodService.createFood()
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(400).json({error: error.message});
+        }else {
+            res.status(500).json({error: "Internal server error"});
+        }
+    }
+}
+
+const deleteItem = async(req,res) => {
+    try {
+        const { id } = req.params;
+
+        const user = req.user;
+        await foodService.deleteFood(id);
+        res.status(200).json({message: "Menu item deleted"});
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(400).json({error: error.message});
+    }else {
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+}
+
+const getMenuItemByName = async(req, res) => {
+    try {
+        const { name } = req.query;
+        const menuItem = await foodService.searchFood(name);
+        res.status(200).json(menuItem);
+    } catch (error) {
+        res.status(500).json({error: "Internal server error "});
+    }
+}
+
+const updateAvilibalityStatus = async(req, res) =>  {
+    try {
+        const { id } = req.params;
+        const menuItem = await foodService.updateAvilibalityStatus(id);
+        res.status(200).json(menuItem);
+    }catch (error) {
+        if(error instanceof Error){
+            res.status(400).json({error: error.message});
+    }else {
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+}
---
+++ src/services/foodService.js
@@ -0,0 +1,115 @@
+ const Food = require('../models/food.model.js')
+
+ const createFood = async(req,restaurant) => {
+    try {
+        const food = new Food({
+            foodCategory: req.category,
+            creationDate: new Date(),
+            description: req.description,
+            images: req.images,
+            name: req.name,
+            price: req.price,
+            isSeasonal: req.seasonal,
+            isVegetarian: req.vegetarian,
+            restaurant: restaurant._id,
+            ingredients: req.ingredients,
+        });
+        await food.save();
+        restaurant.foods.push(food._id);
+        await restaurant.save();
+        return food;
+    } catch (error) {
+        throw new Error(`Failed to create food:${error.message}`);
+    }
+ }
+
+const  deleteFood =async(foodId) => {
+    try {
+        const food = await Food.findById(foodId);
+        if(!food){
+            throw new Error(`Food not found with ID ${foodId}`);
+        }
+        food.restaurant = null;
+        await food.save();
+        await Food.findByIdAndDelete(foodId);
+    } catch (error) {
+        throw new Error(
+            `Failed to delete food with ID ${foodId}: ${error.message}`
+        );
+    }
+}
+
+const getRestaurantFood =async(restaurantId, vegetarian, nonveg, seasonal, foodCategory) => {
+    try {
+        let query = { restaurant: restaurantId};
+        console.log(nonveg)
+        if(vegetarian == 'true'){
+            query.isVegetarian = true;
+        }
+
+        if(nonveg == 'true') query.vegetarian = false;
+        if(seasonal == 'true') query.isSeasonal = true;
+        if (foodCategory)query.foodCategory = foodCategory;
+
+        const foods = await Food.find(query).populate([
+            { path: "ingredients", populate: {path: "category", select:"name"}},
+        "foodCategory",
+        { path: "restuarant", select:"name_id"},
+        ]);
+        return foods;
+
+    } catch (error) {
+        throw new Error(`Failed to retrieve restaurant food: ${error.message}`);
+    }
+}
+
+const searchFood = async(keyword) => {
+    try {
+        let query = {};
+        if(keyword){
+            query.$or = [
+                {name: {$regex: keyword, $options:'i'}},
+                {"foodCategory.name":{$regex: keyword, $options:'i'}},
+            ];
+        }
+
+        const foods = await Food.find(query);
+        return foods;
+    } catch (error) {
+        throw new Error(`Failed to search for food: ${error.message}`);
+    }
+}
+
+const updateAvailibilityStatus = async(foodId) => {
+    try {
+        const food = await Food.findById(foodId).populate([
+            {path:"ingredients", populate: {path: "category", select:'name'}},
+            "foodCategory",
+            { path: "restaurant", select:"name_id"},
+        ]);
+        if(!food){
+            throw new Error(`Food not found with ID ${foodId}`);
+        }
+        food.available = !food.available;
+        await food.save();
+        return food;
+    } catch (error) {
+        throw new Error(
+            `Failed to update availability status for food with ID ${foodId}:${error.message}`
+        )
+    }
+}
+
+const findFoodById = async(foodId){
+    try{
+        const food = await Food.findById(foodId);
+        if(!food){
+            throw new Error(`Food not found with ID ${foodId}`);
+        }
+        return food;
+    } catch (error) {
+        throw new Error(
+            `Failed to find Food with Id ${foodId}: ${error.message}`
+        );
+    }
+};
